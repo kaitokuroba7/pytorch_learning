920cfe9ab38e1692a8a7f6c34245f5a03668a5c9
J.Zhang_PC <zhangjie0209@zju.edu.cn>
2021-03-29 11:55:35 +0800

    learn_GRU_basic_Code

--- 6. 循环神经网络/6.5 循环神经网络的简洁实现/GRU_Func.py
+++ 6. 循环神经网络/6.5 循环神经网络的简洁实现/GRU_Func.py
@@ -12,21 +12,24 @@
 import torch
 import numpy as np
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-num_inputs, num_hiddens, num_outputs = vocab_size, 256, vocab_size
+
+# 定义隐含层状态初始函数
+def init_gru_state(batch_size, num_hiddens, device):
+    return (torch.zeros((batch_size, num_hiddens), device=device),)
 
 
-def get_params():
-    def _one(shape):
-        ts = torch.tensor(np.random.normal(0, 0.01, size=shape), device=device, dtype=torch.float32)
-        return torch.nn.Parameter(ts, requires_grad=True)
-
-    def _three():
-        return (_one((num_inputs, num_hiddens)),
-                _one((num_hiddens, num_hiddens)),
-                torch.nn.Parameter(torch.zeros(num_hiddens, device=device, dtype=torch.float32), requires_grad=True)
-                )
-
+def gru(inputs, state, params):
+    W_xz, W_hz, b_z, W_xr, W_hr, b_r, W_xh, W_hh, b_h, W_hq, b_q = params
+    H, = state
+    outputs = []
+    for X in inputs:
+        Z = torch.sigmoid(torch.matmul(X, W_xz) + torch.matmul(H, W_hz) + b_z)
+        R = torch.sigmoid(torch.matmul(X, W_xr) + torch.matmul(H, W_hr) + b_r)
+        H_tilda = torch.tanh(torch.matmul(X, W_xh) + torch.matmul(H, W_hh) + b_h)
+        H = Z * H + (1 - Z) * H_tilda
+        Y = torch.matmul(H, W_hq) + b_q
+        outputs.append(Y)
+    return outputs, (H,)
 
 
 if __name__ == "__main__":
--- 6. 循环神经网络/6.5 循环神经网络的简洁实现/GRU_main.py
+++ 6. 循环神经网络/6.5 循环神经网络的简洁实现/GRU_main.py
@@ -14,17 +14,45 @@ import torch
 from torch import nn, optim
 import torch.nn.functional as F
 import Function.utils as d2l
+import GRU_Func as Func
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 (corpus_indices, char_to_idx, idx_to_char, vocab_size) = d2l.load_data_jay_lyrics()
 
-
 num_inputs, num_hiddens, num_outputs = vocab_size, 256, vocab_size
 print('will use', device)
 
 
+def get_params():
+    def _one(shape):
+        ts = torch.tensor(np.random.normal(0, 0.01, size=shape), device=device, dtype=torch.float32)
+        return torch.nn.Parameter(ts, requires_grad=True)
+
+    def _three():
+        return (_one((num_inputs, num_hiddens)),
+                _one((num_hiddens, num_hiddens)),
+                torch.nn.Parameter(torch.zeros(num_hiddens, device=device, dtype=torch.float32), requires_grad=True)
+                )
+
+    W_xz, W_hz, b_z = _three()  # 更新门参数
+    W_xr, W_hr, b_r = _three()  # 重置门参数
+    W_xh, W_hh, b_h = _three()  # 候选隐藏状态参数
+
+    # 输出层参数
+    W_hq = _one((num_hiddens, num_outputs))
+    b_q = torch.nn.Parameter(torch.zeros(num_outputs, device=device, dtype=torch.float32), requires_grad=True)
+    return nn.ParameterList([W_xz, W_hz, b_z, W_xr, W_hr, b_r, W_xh, W_hh, b_h, W_hq, b_q])
 
 
 if __name__ == "__main__":
+    res = get_params()
+    print(res)
+    num_epochs, num_steps, batch_size, lr, clipping_theta = 160, 35, 32, 1e2, 1e-2
+    pred_period, pred_len, prefixes = 40, 50, ['分开', '不分开']
+    d2l.train_and_predict_rnn(Func.gru, get_params, Func.init_gru_state, num_hiddens,
+                              vocab_size, device, corpus_indices, idx_to_char,
+                              char_to_idx, False, num_epochs, num_steps, lr,
+                              clipping_theta, batch_size, pred_period, pred_len,
+                              prefixes)
     pass
